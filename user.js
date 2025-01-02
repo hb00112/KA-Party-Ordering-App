d58c8076e5fae63aab62ea2b901bee5a9980411c
hb00112 <159218387+hb00112@users.noreply.github.com>
2025-01-02 19:12:58 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
 const validUsers = {
-    '123': { password: '123', username: 'ABC COMPANY' },
-    '456': { password: '456', username: 'HC PVT LTD' },
-    'HIMANSHU': { password: 'HIMANSHU', username: 'HC PVT LTD' }
+    '123': { password: '123', username: 'Hreenkar Creation' },
+    '456': { password: '456', username: 'Hreenkar Creation' },
+    'HIMANSHU': { password: 'HIMANSHU', username: 'Hreenkar Creation' }
 };
