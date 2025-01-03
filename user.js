8d8786b85726b0e13937e1ae360c158e5fa334b7
hb00112 <159218387+hb00112@users.noreply.github.com>
2025-01-03 14:58:29 +0530

    Delete user.js

--- user.js
+++
@@ -1,8 +0,0 @@
-const validUsers = {
-    '123': { password: '123', username: 'ABC COMPANY' },
-    '456': { password: '456', username: 'HC PVT LTD' },
-    'HIMANSHU': { password: 'HIMANSHU', username: 'Hreenkar Creation' },
-      'H': { password: 'H', username: 'HEMANT BORANA' }
-};
-
-// Add this to your main JavaScript file
