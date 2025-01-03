ce3dbbbcb09e9866aa6638d70e845c1d1e7657a5
hb00112 <159218387+hb00112@users.noreply.github.com>
2025-01-03 07:47:46 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,5 @@
+const validUsers = {
+    '123': { password: '123', username: 'ABC COMPANY' },
+    '456': { password: '456', username: 'HC PVT LTD' },
+    'HIMANSHU': { password: 'HIMANSHU', username: 'Hreenkar Creation' }
+};
