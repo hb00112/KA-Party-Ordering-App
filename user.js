f4a53a52937d7a2d518968b172291a89706265e4
hb00112 <159218387+hb00112@users.noreply.github.com>
2025-01-03 07:46:50 +0530

    Delete user.js

--- user.js
+++
@@ -1,5 +0,0 @@
-const validUsers = {
-    '123': { password: '123', username: 'Hreenkar Creation' },
-    '456': { password: '456', username: 'Hreenkar Creation' },
-    'HIMANSHU': { password: 'HIMANSHU', username: 'Hreenkar Creation' }
-};
